a01eb69ff6182becd04292015f8f3412106343f2
lihaha <229519682@qq.com>
2020-07-22 10:54:06 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,17 @@
+"""
+print("hello world!")   #字符串
+print("哈哈"+"xixi")
+print("haha"*10)
+print(233)    #整数
+print(2.33)   #小数
+print(True)   #布尔值
+print(())    #元组
+print([])   #数组
+print({})   #字典
+
+锄禾日当午
+汗滴禾下土
+"""
+
+a = input("请输入：")
+print("input获取的内容：",a)
